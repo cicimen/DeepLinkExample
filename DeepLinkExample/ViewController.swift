449d89ccb8cd3bd3c8cfa93f9adb978ff578d20e
cicimen <egemengulkilik@gmail.com>
2020-07-19 22:01:44 +0300

    Initial Commit

---
+++ DeepLinkExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DeepLinkExample
+//
+//  Created by Egemen on 19.07.2020.
+//  Copyright © 2020 Egemen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
