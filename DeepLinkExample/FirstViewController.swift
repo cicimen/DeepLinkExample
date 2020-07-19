f293fc746887d73a1cfc07abe7dcac0f199167bd
cicimen <egemengulkilik@gmail.com>
2020-07-19 22:15:37 +0300

    Initial Commir

--- DeepLinkExample/FirstViewController.swift
+++ DeepLinkExample/FirstViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  FirstViewController.swift
 //  DeepLinkExample
 //
 //  Created by Egemen on 19.07.2020.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class FirstViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
---
+++ DeepLinkExample/SecondViewController.swift
@@ -0,0 +1,9 @@
+//
+//  SecondViewController.swift
+//  DeepLinkExample
+//
+//  Created by Egemen on 19.07.2020.
+//  Copyright © 2020 Egemen. All rights reserved.
+//
+
+import Foundation
